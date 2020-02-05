c1494e41c706f73e19a73e63b2e8a791ebdedd92
wjz23 <924731303@qq.com>
2020-02-05 10:32:06 +0800

    first

--- src/main/java/com/mforum/HelloController.java
+++ src/main/java/com/mforum/HelloController.java
@@ -3,15 +3,13 @@ package com.mforum;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
     @GetMapping("/greeting")
     public String sayHello(@RequestParam(name = "name",required = false,defaultValue = "world")String name, Model model){
         model.addAttribute("name",name);
-        return "hello";
+        return "index";
     }
 }
