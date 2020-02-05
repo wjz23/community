06f188353f13931314f1bbc0be2648b19dd04e67
wjz23 <924731303@qq.com>
2020-02-05 09:06:49 +0800

    init

---
+++ src/main/java/com/mforum/MforumApplication.java
@@ -0,0 +1,13 @@
+package com.mforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mforum/MforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
